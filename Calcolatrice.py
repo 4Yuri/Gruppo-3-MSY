3ab4db1a23219e3bb015aadf13fdf486babab4b2
4Yuri <89109005+4Yuri@users.noreply.github.com>
2026-06-12 17:34:03 +0200

    Update Calcolatrice.py
    Updated comments and improved code readability.

--- Calcolatrice.py
+++ Calcolatrice.py
@@ -1,27 +1,27 @@
 class Calcolatrice:
     
     def __init__(self):
-        # Inizializzo le liste vuote per tenere a mente i calcoli
+        # Inizializzo le liste vuote per memorizzare i calcoli
         self.risultati = []
         self.valori_inseriti = []
-        # Imposto il contatore a zero e ricordo che posso fare al massimo 4 operazioni
+        # Imposto il contatore a zero e definisco il limite massimo di 4 operazioni
         self.numero_operazioni = 0
         self.max_operazioni = 4
         
     def addizione(self, lista):
-        # Parto da zero e sommo tutti i numeri che mi hai passato
+        # Parto da zero e sommo tutti i numeri passati
         risultato = 0
         for n in lista:
             risultato += n
-        # Salvo i numeri usati e il risultato finale nella mia memoria
+        # Salvo i numeri usati e il risultato finale
         self.valori_inseriti.append(lista)
         self.risultati.append(risultato)
-        # Tengo conto che ho appena fatto un'operazione
+        # Incremento il contatore delle operazioni eseguite
         self.numero_operazioni += 1
         return risultato
     
     def sottrazione(self, lista):
-        # Se non mi dai numeri, non posso fare nulla
+        # Se la lista è vuota, restituisco 0
         if len(lista) == 0:
             return 0
         # Parto dal primo numero e sottraggo tutti gli altri in sequenza
@@ -39,19 +39,20 @@ class Calcolatrice:
         risultato = 1
         for n in lista:
             risultato *= n
-        # Salvo tutto nella mia memoria e aggiorno il contatore
+        # Salvo i dati e aggiorno il contatore
         self.valori_inseriti.append(lista)
         self.risultati.append(risultato)
         self.numero_operazioni += 1
         return risultato
     
     def divisione(self, lista):
+        # Se la lista è vuota, restituisco 0
         if len(lista) == 0:
             return 0
         # Parto dal primo numero e divido per gli altri
         risultato = lista[0]
         for i in range(1, len(lista)):
-            # Controllo che nessuno mi chieda di dividere per zero
+            # Controllo che non ci siano divisioni per zero
             if lista[i] == 0:
                 print("Errore: divisione per zero")
                 return None
@@ -63,13 +64,14 @@ class Calcolatrice:
         return risultato
     
     def media(self, lista):
+        # Se la lista è vuota, restituisco 0
         if len(lista) == 0:
             return 0
-        # Prima sommo tutti i numeri che mi hai dato
+        # Prima sommo tutti i numeri presenti nella lista
         somma = 0
         for n in lista:
             somma += n
-        # Poi li divido per la quantità totale per trovare la media
+        # Poi divido la somma per la quantità totale per trovare la media
         risultato = somma / len(lista)
         # Memorizzo i valori e il risultato della media
         self.valori_inseriti.append(lista)
@@ -78,8 +80,8 @@ class Calcolatrice:
         return risultato
         
     def stampa_storico(self):
-        # Scorro la mia memoria e ti mostro a video tutte le operazioni che ho fatto
+        # Scorro la memoria e stampo a video tutte le operazioni eseguite
         for i in range(len(self.valori_inseriti)):
             print(str(i + 1) + ". Valori: " + str(self.valori_inseriti[i]) + " -> Risultato: " + str(self.risultati[i]))
-        # Ti dico quante operazioni ho consumato rispetto al mio limite
+        # Mostro quante operazioni ho consumato rispetto al limite massimo
         print("Operazioni fatte: " + str(self.numero_operazioni) + "/" + str(self.max_operazioni))
